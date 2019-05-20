5686d0aff3ac03ab5b5e1e10d584997406b9b151
liulinboyi <814921718@qq.com>
2019-05-21 00:31:18 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import './import-jquery.js';
-import './import-art';
+// import './import-art';
 import '../node_modules/bootstrap/dist/js/bootstrap.js';
 // import './import-layui';
 // import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
